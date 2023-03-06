779434d802de39a46ef7a1d959cf8ec03bb11728
Ricardo <97.rramirez@gmail.com>
2023-03-06 17:22:57 -0300

    currency

---
+++ custom.d.ts
@@ -0,0 +1,25 @@
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.json" {
+  const value: string;
+  export default value;
+}
